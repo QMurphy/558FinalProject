db1bea0983aaebb874db13c7402456d6c03f2fc2
Colin <colinw@iastate.edu>
2016-12-04 14:58:06 -0600

    parsing works pt 3

--- src/edu/iastate/Main.java
+++ src/edu/iastate/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
-        InputParser ip = new InputParser(new File("src/edu/iastate/input.txt"));
+        InputParser ip = new InputParser(new File("input.txt"));
         List<Task> tasks = ip.parseInput();
         for(Task t : tasks){
             System.out.println(t.getID() + " " + t.getCompTime() + " " + t.getPeriod());
